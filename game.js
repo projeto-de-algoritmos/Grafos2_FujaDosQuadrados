a1d3992f743707093cb07188f2e1c385c50300c9
brenno-silva <brenno.silva037@gmail.com>
2022-02-18 16:10:14 -0300

    Adicionando player

--- game.js
+++ game.js
@@ -1,15 +1,16 @@
-const COLORS = ['black', 'lightgray', 'yellow', 'orange', 'red', 'blue']
+const COLORS = ['black', 'lightgray', 'yellow', 'orange', 'red', 'blue', 'purple']
 const SLOW_TIME = [2, 5, 10]
 const WIDTH = 28;
 const HEIGTH = 31;
-const TIMEOUT = 200;
+const TIMEOUT = 150;
 
-const jogadores = [
+const inimigos = [
     createInimigo(1, 1),
     createInimigo(1, 26),
-    createInimigo(29, 26),
-    createInimigo(29, 1),
+    createInimigo(29, 26)
 ]
+
+const player = createPlayer(28, 1);
 
 function createInimigo(x, y) {
     return {
@@ -24,17 +25,91 @@ function createInimigo(x, y) {
     }
 }
 
+function createPlayer(x, y) {
+    return {
+        x: x,
+        y: y,
+        slow: 0,
+        color: 6,
+        vel: {
+            x: -1,
+            y: 0,
+        }
+    }
+}
+
 function start() {
     renderMap();
     setInterval(() => {
-        updateInimigos();
+        updateInimigosList();
         renderMap();
         renderInimigos();
+        updatePlayer(player);
+        renderPlayer(player);
     }, TIMEOUT);
 }
 
-function updateInimigos() {
-    jogadores.forEach(j => updateInimigo(j))
+function updateInimigosList() {
+    inimigos.forEach(j => updateInimigo(j))
+}
+
+function updatePlayer(jogador) {
+    const colorIndex = map[jogador.x][jogador.y]
+
+    if (colorIndex > 1) {
+        //console.log(jogador.slow)
+        if (jogador.slow === 0) {
+            jogador.slow = getSlowTime(colorIndex)
+            return;
+        } else {
+            jogador.slow--;
+            if (jogador.slow > 0)
+                return;
+        }
+    }
+
+    let y = jogador.y + jogador.vel.y
+    let x = jogador.x + jogador.vel.x
+    let nextBlock = map[x][y];
+
+    if (!isBlockFree(nextBlock)) {
+        jogador.vel.x = 0;
+        jogador.vel.y = 0;
+    }
+
+    document.addEventListener('keyup', (event) => {
+        switch (event.code) {
+            case "ArrowUp":
+                if (isBlockFree(nextBlock)) {
+                    jogador.vel.x = -1;
+                    jogador.vel.y = 0;
+                }
+                break;
+            case "ArrowDown":
+                if (isBlockFree(nextBlock)) {
+                    jogador.vel.x = 1;
+                    jogador.vel.y = 0;
+                }
+                break;
+            case "ArrowLeft":
+                if (isBlockFree(nextBlock)) {
+                    jogador.vel.x = 0;
+                    jogador.vel.y = -1;
+                }
+                break;
+            case "ArrowRight":
+                if (isBlockFree(nextBlock)) {
+                    jogador.vel.x = 0;
+                    jogador.vel.y = 1;
+                }
+                break;
+            default:
+                break;
+        }
+    }, false);
+
+    jogador.x += jogador.vel.x;
+    jogador.y += jogador.vel.y;
 }
 
 function updateInimigo(jogador) {
@@ -129,10 +204,15 @@ function renderMap() {
 function getID(x, y) {
     return `block-${x}-${y}`
 }
+
 function renderInimigos() {
-    jogadores.forEach(j => {
+    inimigos.forEach(j => {
         window.document.querySelector(`#${getID(j.x, j.y)}`).style.backgroundColor = COLORS[j.color]
     })
+}
+
+function renderPlayer(jogador) {
+    window.document.querySelector(`#${getID(jogador.x, jogador.y)}`).style.backgroundColor = COLORS[jogador.color]
 }
 
 start()
