085eff7739e5640e7e22408737e5299bba8a9dbe
brenno-silva <brenno.silva037@gmail.com>
2022-02-19 11:13:32 -0300

    Adicionando classes

--- game.js
+++ game.js
@@ -4,176 +4,6 @@ const WIDTH = 28;
 const HEIGTH = 31;
 const TIMEOUT = 150;
 
-const inimigos = [
-    createInimigo(1, 1),
-    createInimigo(1, 26),
-    createInimigo(29, 26)
-]
-
-const player = createPlayer(28, 1);
-
-function createInimigo(x, y) {
-    return {
-        x: x,
-        y: y,
-        slow: 0,
-        color: 5,
-        vel: {
-            x: 1,
-            y: 0,
-        }
-    }
-}
-
-function createPlayer(x, y) {
-    return {
-        x: x,
-        y: y,
-        slow: 0,
-        color: 6,
-        vel: {
-            x: -1,
-            y: 0,
-        }
-    }
-}
-
-function start() {
-    renderMap();
-    setInterval(() => {
-        updateInimigosList();
-        renderMap();
-        renderInimigos();
-        updatePlayer(player);
-        renderPlayer(player);
-    }, TIMEOUT);
-}
-
-function updateInimigosList() {
-    inimigos.forEach(j => updateInimigo(j))
-}
-
-function updatePlayer(jogador) {
-    const colorIndex = map[jogador.x][jogador.y]
-
-    if (colorIndex > 1) {
-        //console.log(jogador.slow)
-        if (jogador.slow === 0) {
-            jogador.slow = getSlowTime(colorIndex)
-            return;
-        } else {
-            jogador.slow--;
-            if (jogador.slow > 0)
-                return;
-        }
-    }
-
-    let y = jogador.y + jogador.vel.y
-    let x = jogador.x + jogador.vel.x
-    let nextBlock = map[x][y];
-
-    if (!isBlockFree(nextBlock)) {
-        jogador.vel.x = 0;
-        jogador.vel.y = 0;
-    }
-
-    document.addEventListener('keyup', (event) => {
-        switch (event.code) {
-            case "ArrowUp":
-                if (isBlockFree(nextBlock)) {
-                    jogador.vel.x = -1;
-                    jogador.vel.y = 0;
-                }
-                break;
-            case "ArrowDown":
-                if (isBlockFree(nextBlock)) {
-                    jogador.vel.x = 1;
-                    jogador.vel.y = 0;
-                }
-                break;
-            case "ArrowLeft":
-                if (isBlockFree(nextBlock)) {
-                    jogador.vel.x = 0;
-                    jogador.vel.y = -1;
-                }
-                break;
-            case "ArrowRight":
-                if (isBlockFree(nextBlock)) {
-                    jogador.vel.x = 0;
-                    jogador.vel.y = 1;
-                }
-                break;
-            default:
-                break;
-        }
-    }, false);
-
-    jogador.x += jogador.vel.x;
-    jogador.y += jogador.vel.y;
-}
-
-function updateInimigo(jogador) {
-    const colorIndex = map[jogador.x][jogador.y]
-
-    if (colorIndex > 1) {
-        //console.log(jogador.slow)
-        if (jogador.slow === 0) {
-            jogador.slow = getSlowTime(colorIndex)
-            return;
-        } else {
-            jogador.slow--;
-            if (jogador.slow > 0)
-                return;
-        }
-    }
-
-    let y = jogador.y + jogador.vel.y
-    let x = jogador.x + jogador.vel.x
-    let nextBlock = map[x][y];
-
-    if (possiblePaths(jogador) > 2)
-        nextBlock = randomPlayerPosition(jogador);
-
-    while (!isBlockFree(nextBlock))
-        nextBlock = randomPlayerPosition(jogador);
-
-    jogador.x += jogador.vel.x;
-    jogador.y += jogador.vel.y;
-}
-
-function possiblePaths(jogador) {
-    let paths = 0;
-
-    if (isBlockFree(map[jogador.x + 1][jogador.y]))
-        paths++;
-    if (isBlockFree(map[jogador.x - 1][jogador.y]))
-        paths++;
-    if (isBlockFree(map[jogador.x][jogador.y + 1]))
-        paths++;
-    if (isBlockFree(map[jogador.x][jogador.y - 1]))
-        paths++;
-
-    return paths;
-}
-
-function randomPlayerPosition(jogador) {
-    if (getRandomBool()) {
-        jogador.vel.x = getRandomBool() ? 1 : -1;
-        jogador.vel.y = 0;
-    } else {
-        jogador.vel.x = 0;
-        jogador.vel.y = getRandomBool() ? 1 : -1;
-    }
-
-    y = jogador.y + jogador.vel.y;
-    x = jogador.x + jogador.vel.x;
-    return map[x][y];
-}
-
-function getSlowTime(colorIndex) {
-    return SLOW_TIME[colorIndex - 2];
-}
-
 function getRandomBool() {
     return getRandomInt(0, 2) === 0;
 }
@@ -184,8 +14,184 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function isBlockFree(block) {
-    return block > 0;
+class Player {
+
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.slow = 0;
+        this.color = color;
+        this.vel = { x, y }
+        this.vel.x = 1;
+        this.vel.y = 0;
+    }
+
+    updatePlayer(map) {
+        const colorIndex = map[this.x][this.y]
+
+        if (colorIndex > 1) {
+            if (this.slow === 0) {
+                this.slow = this.getSlowTime(colorIndex);
+                return;
+            } else {
+                this.slow--;
+                if (this.slow > 0)
+                    return;
+            }
+        }
+
+        let y = this.y + this.vel.y;
+        let x = this.x + this.vel.x;
+        let nextBlock = map[x][y];
+
+        if (!this.isBlockFree(nextBlock)) {
+            this.vel.x = 0;
+            this.vel.y = 0;
+        }
+
+        document.addEventListener('keyup', (event) => {
+            switch (event.code) {
+                case "ArrowUp":
+                    if (this.isBlockFree(nextBlock)) {
+                        this.vel.x = -1;
+                        this.vel.y = 0;
+                    }
+                    break;
+                case "ArrowDown":
+                    if (this.isBlockFree(nextBlock)) {
+                        this.vel.x = 1;
+                        this.vel.y = 0;
+                    }
+                    break;
+                case "ArrowLeft":
+                    if (this.isBlockFree(nextBlock)) {
+                        this.vel.x = 0;
+                        this.vel.y = -1;
+                    }
+                    break;
+                case "ArrowRight":
+                    if (this.isBlockFree(nextBlock)) {
+                        this.vel.x = 0;
+                        this.vel.y = 1;
+                    }
+                    break;
+                default:
+                    break;
+            }
+        }, false);
+
+        this.x += this.vel.x;
+        this.y += this.vel.y;
+    }
+
+    isBlockFree(block) {
+        return block > 0;
+    }
+
+    getSlowTime(colorIndex) {
+        return SLOW_TIME[colorIndex - 2];
+    }
+}
+
+class Enemy {
+
+    constructor(x, y, color) {
+        this.x = x;
+        this.y = y;
+        this.slow = 0;
+        this.color = color;
+        this.vel = { x, y }
+        this.vel.x = 1;
+        this.vel.y = 0;
+    }
+
+    updateEnemy(map) {
+        const colorIndex = map[this.x][this.y]
+
+        if (colorIndex > 1) {
+            if (this.slow === 0) {
+                this.slow = this.getSlowTime(colorIndex)
+                return;
+            } else {
+                this.slow--;
+                if (this.slow > 0)
+                    return;
+            }
+        }
+
+        let y = this.y + this.vel.y
+        let x = this.x + this.vel.x
+        let nextBlock = map[x][y];
+
+        if (this.possiblePaths(map) > 2)
+            nextBlock = this.randomEnemyPosition(map);
+
+        while (!this.isBlockFree(nextBlock))
+            nextBlock = this.randomEnemyPosition(map);
+
+        this.x += this.vel.x;
+        this.y += this.vel.y;
+    }
+
+    possiblePaths(map) {
+        let paths = 0;
+
+        if (this.isBlockFree(map[this.x + 1][this.y]))
+            paths++;
+        if (this.isBlockFree(map[this.x - 1][this.y]))
+            paths++;
+        if (this.isBlockFree(map[this.x][this.y + 1]))
+            paths++;
+        if (this.isBlockFree(map[this.x][this.y - 1]))
+            paths++;
+
+        return paths;
+    }
+
+    isBlockFree(block) {
+        return block > 0;
+    }
+
+    randomEnemyPosition(map) {
+        if (getRandomBool()) {
+            this.vel.x = getRandomBool() ? 1 : -1;
+            this.vel.y = 0;
+        } else {
+            this.vel.x = 0;
+            this.vel.y = getRandomBool() ? 1 : -1;
+        }
+
+        const y = this.y + this.vel.y;
+        const x = this.x + this.vel.x;
+        return map[x][y];
+    }
+
+    getSlowTime(colorIndex) {
+        return SLOW_TIME[colorIndex - 2];
+    }
+}
+
+const inimigos = [
+    new Enemy(1, 1, 5),
+    new Enemy(1, 26, 5),
+    new Enemy(29, 26, 5)
+]
+
+const player = new Player(28, 1, 6);
+
+function start() {
+    renderMap();
+    setInterval(() => {
+        updateInimigosList();
+        renderMap();
+        renderInimigos();
+        player.updatePlayer(map);
+        renderPlayer(player);
+    }, TIMEOUT);
+}
+
+function updateInimigosList() {
+    inimigos.forEach(j => j.updateEnemy(map))
 }
 
 function renderMap() {
